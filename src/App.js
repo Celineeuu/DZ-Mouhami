1f607195e5f0a8a9d2d63dd56892909813a2ac95
4m1ra123 <127878136+4m1ra123@users.noreply.github.com>
2023-12-23 19:45:08 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Route , BrowserRouter as Router , Routes } from "react-router-dom";
 import Inscription from "./Inscription";
 import PrendreRendezVous from "./prendreRendezVous";
-
+import Connexion from "./Connexion";
 const App = () => {
     return ( 
         <Router>
@@ -10,6 +10,7 @@ const App = () => {
            <Routes>
              <Route path="/Inscription" element={<Inscription />}></Route>
              <Route path="/PrendreRendezVous" element={<PrendreRendezVous />}></Route>
+     <Route path="/Connexion" element={<Connexion />}></Route>
            </Routes>   
          </div>
         </Router>    
